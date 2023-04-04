f4f5cb3950b28c505e0305396c259c4438c228c9
fayizmdt <fayizkunhalan@gmail.com>
2023-04-04 13:58:52 +0300

    3

---
+++ src/Table.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import students from "./students.json";
+import "./Table.css";
+
+function Table() {
+    const [rows, setRows] = useState(students);
+    const [newStudent, setNewStudent] = useState({ id: "", name: "", age: "", grade: "" });
+
+    const handleAddStudent = (event) => {
+        event.preventDefault();
+        setRows([...rows, newStudent]);
+        setNewStudent({ id: "", name: "", age: "", grade: "" });
+    };
+
+    const columns = [
+        { key: "id", name: "ID" },
+        { key: "name", name: "Name" },
+        { key: "age", name: "Age" },
+        { key: "grade", name: "Grade" },
+    ];
+
+    return (
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        {columns.map((column) => (
+                            <th key={column.key}>{column.name}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {rows.map((student) => (
+                        <tr key={student.id}>
+                            {columns.map((column) => (
+                                <td key={column.key}>{student[column.key]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <form onSubmit={handleAddStudent}>
+                <input
+                    type="text"
+                    placeholder="ID"
+                    value={newStudent.id}
+                    onChange={(event) =>
+                        setNewStudent({ ...newStudent, id: event.target.value })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={newStudent.name}
+                    onChange={(event) =>
+                        setNewStudent({ ...newStudent, name: event.target.value })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="Age"
+                    value={newStudent.age}
+                    onChange={(event) =>
+                        setNewStudent({ ...newStudent, age: event.target.value })
+                    }
+                />
+                <input
+                    type="text"
+                    placeholder="Grade"
+                    value={newStudent.grade}
+                    onChange={(event) =>
+                        setNewStudent({ ...newStudent, grade: event.target.value })
+                    }
+                />
+                <button type="submit">Add Student</button>
+            </form>
+        </div>
+    );
+}
+
+export default Table;
